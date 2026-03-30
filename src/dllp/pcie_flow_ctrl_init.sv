eb03c566689800095ba3a6d2a58ef080d60ac3bf
Max Wierse <mwierse@student.ethz.ch>
2026-03-30 17:45:25 +0200

    More debugging probes

--- src/dllp/pcie_flow_ctrl_init.sv
+++ src/dllp/pcie_flow_ctrl_init.sv
@@ -72,7 +72,7 @@ module pcie_flow_ctrl_init
   logic                                 fc_axis_tready;
 
   // Internal state machine for link flow control
-  flow_control_state_e                  curr_state;
+   (* syn_keep = "true", mark_debug = "true" *)  flow_control_state_e                  curr_state;
   flow_control_state_e                  next_state;
   dllp_fc_t                             dll_packet_c;
   dllp_fc_t                             dll_packet_r;
